7923cfc822eb9e5f802050c450a057f416e1be2c
Gokulchilukuri <gokulprasadguptha@gmail.com>
2025-05-01 01:41:40 +0530

    Add

--- hello.sh
+++ hello.sh
@@ -1,8 +1,2 @@
-<<<<<<< HEAD
 echo 'hello man'
 touch g
-=======
-echo 'hello'
-touch g,e
-cd g
->>>>>>> b1
--- test.py
+++ test.py
@@ -1,7 +1,2 @@
 print('hello')
-<<<<<<< HEAD
 int=()
-=======
-int=(i)
-print(int(i))
->>>>>>> b1
